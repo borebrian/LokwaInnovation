bb1ac0cda9ab6145c5d341d80e545cbb04b6d961
Bore Brian <borebrian77@gmail.com>
2020-09-21 00:37:14 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LokwaInnovation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
